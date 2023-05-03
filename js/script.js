d0b33ef241b5a7cc1dac3de454e7176e1ea161f1
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-05-03 22:55:10 +0000

    added html

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+
+// This program will check your age and tell you whether or not you can watch certain movies
+
+function checkAge() {
+  // Declare variables.
+  const age = parseInt(document.getElementById("age").value)
+
+  // Check age and tell user if they can watch the movie.
+  if (age >= 18) {
+    document.getElementById('answer').innerHTML = "You can watch R rated movies alone."
+  } else if (age >= 13) {
+    document.getElementById('answer').innerHTML = "You can watch PG rated movies alone."
+  } else {
+    document.getElementById('answer').innerHTML = "You can watch G rated movies alone."
+  }
+}
